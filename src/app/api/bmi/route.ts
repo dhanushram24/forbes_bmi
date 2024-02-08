a3d2f926c177fd2395ba7551a7de440d2d82ab6c
dhanushram24 <guttuladhanush.ram@vitstudent.ac.in>
2024-02-08 12:53:37 +0530

    first commit

---
+++ src/app/api/bmi/route.ts
@@ -0,0 +1,18 @@
+export async function POST(req) {
+  const body = await req.json();
+  console.log(body);
+  const isInches=body.isInches;
+  let m;
+  let kg;
+  if(isInches){
+	m=(parseInt(body.feet,10)*12+parseInt(body.heightInInches,10))*0.0254;
+	kg=parseInt(body.weight)*0.45359237;
+  }
+  else{
+	m=parseFloat(body.heightInCm)*0.01;
+	kg=parseInt(body.weight);
+  }
+
+  const bmi = parseFloat(kg) / (m * m);
+  return Response.json(bmi);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Calculator from './components/Calculator/calculator';
+//import ChartContainer from './components/ChartContainer/chartcontainer'
+export default function Home() {
+  return(
+    <>
+    <Calculator/>
+   
+    </>
+  )
+}
