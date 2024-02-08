65a780b4bdc13493fd1e636287c985068fce614d
dhanushram24 <guttuladhanush.ram@vitstudent.ac.in>
2024-02-08 15:37:38 +0530

    fixed changes

--- src/app/components/Calculator/calculator.jsx
+++ src/app/components/Calculator/calculator.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import styles from './Calculator.module.css';
+import styles from './calculator.module.css';
 import Image from 'next/image';
 
 export default function Calculator() {
