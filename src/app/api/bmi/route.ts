999ff12b51c51295f3d0bbfc5d5f9e4325e20434
dhanushram24 <guttuladhanush.ram@vitstudent.ac.in>
2024-02-08 15:20:58 +0530

    fixed changes

--- src/app/api/bmi/route.ts
+++ src/app/api/bmi/route.ts
@@ -1,18 +1,17 @@
 export async function POST(req) {
   const body = await req.json();
-  console.log(body);
   const isInches=body.isInches;
-  let inm;
-  let inkg;
+  let m;
+  let kg;
   if(isInches){
-	inm=(parseInt(body.feet,10)*12+parseInt(body.heightInInches,10))*0.0254;
-	inkg=parseInt(body.weight)*0.45359237;
+	m=(parseInt(body.feet,10)*12+parseInt(body.heightInInches,10))*0.0254;
+	kg=parseInt(body.weight)*0.45359237;
   }
   else{
-	inm=parseFloat(body.heightInCm)*0.01;
-	inkg=parseInt(body.weight);
+	m=parseFloat(body.heightInCm)*0.01;
+	kg=parseInt(body.weight);
   }
 
-  const bmi = parseFloat(inkg) / (inm * inm);
+  const bmi = parseFloat(kg) / (m * m);
   return Response.json(bmi);
 }
